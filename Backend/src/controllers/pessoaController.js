b263a3e3a0144e6a3676479b8af6c203d1f8be0d
github <rafael10elt@gmail.com>
2020-05-09 21:57:19 -0300

    V1

---
+++ Backend/src/controllers/pessoaController.js
@@ -0,0 +1,57 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    //Definindo o método para listar todas as pessoas
+    async index(req, res) {
+        const pessoas = await connection('pessoas').select('*');
+
+        return res.json(pessoas);
+    },
+    //Definindo o método criar para registrar uma nova pessoa
+    async create(req, res) {
+        const { nome, telefone, email } = req.body;
+
+        const { id } = await connection('pessoas').insert({
+            nome,
+            telefone,
+            email,
+        });
+        return res.json({ id });
+    },
+    //Definindo o método delete para apagar o registro de uma pessoa específica
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const pessoas = await connection('pessoas')
+            .where('id', id)
+            .select('id')
+            .first();
+
+        // if (pessoas.id !== id) {
+        //     return res.status(401).json({ error: 'Operation not permitted.' });
+
+        // }
+        await connection('pessoas').where('id', id).delete();
+
+        return res.status(204).send();
+    },
+    //Definindo o método update para editar as informações de uma pessoa específica
+    async update(req, res) {
+        const { id } = req.params;
+        const pessoas = await connection('pessoas')
+            .where('id', id)
+            .select('id')
+            .first();
+
+        // if (pessoas.id !== id) {
+        //     return res.status(401).json({ error: 'Operation not permitted.' });
+
+        // }
+        await connection('pessoas').where('id', id).update({
+            nome: req.body.nome,
+            telefone: req.body.telefone,
+            email: req.body.email,
+        });
+        return res.status(204).send();
+    }
+};
